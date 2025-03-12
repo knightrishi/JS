1cd318874f7cdad156d602d6e59828f3460a1284
knightrishi <arnav.ara05@gmail.com>
2025-03-12 18:57:54 +0000

    conversion

---
+++ JS_1/conversion.js
@@ -0,0 +1,24 @@
+/*let sc="5"
+console.log(typeof sc);
+let num=Number(sc);
+console.log(typeof num);
+console.log(num);
+let un=undefined
+let ch=String(un)
+console.log(typeof un);
+console.log(typeof ch);
+console.log(un);
+console.log(ch);*/
+/**
+ * true==1
+ * null=0
+ * "33"==33
+ * "33av"==NaN
+ * 1==true
+ * "arnav"==true
+ *  ""==false 
+*/
+let lgg=1
+let bool=Boolean(lgg)
+console.log(typeof lgg);
+console.log(bool);
