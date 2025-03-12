84b6566e5b919c0b049c3b01cb7aeab58f948f02
knightrishi <arnav.ara05@gmail.com>
2025-03-12 19:31:51 +0000

    comparison

---
+++ JS_1/comparison.js
@@ -0,0 +1,9 @@
+console.log("2"==2);//t
+console.log("2">=2);//t
+console.log(null>0);//f
+console.log(null==0);//f
+console.log(null>=0);//t
+console.log("2"===2);//=== also checks datatype
+console.log(undefined==0);//f
+console.log(undefined>=0);//f
+console.log(undefined>0);//f
