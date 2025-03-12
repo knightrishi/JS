d930901a59c4db052295ef785c13a7f9570ad81e
knightrishi <arnav.ara05@gmail.com>
2025-03-12 19:21:37 +0000

    conversion

--- JS_1/conversion.js
+++ JS_1/conversion.js
@@ -20,5 +20,21 @@ console.log(ch);*/
 */
 let lgg=1
 let bool=Boolean(lgg)
-console.log(typeof lgg);
-console.log(bool);
+//console.log(typeof lgg);
+//console.log(bool);
+/* ******************************** OPERATION ********************************************************/
+let num1 ,num2,num3 
+console.log(1 +2 +"2");//32
+console.log("1" +2 +2);//122
+console.log(1 +2 );//3
+console.log("1" +2 );//12
+console.log(1 +"2" );//12
+num1=100;
+num3=num1++;//first sone in memory thenn increment
+num2=333
+let num4=++num2//first increment then increase
+console.log(num1);//101
+console.log(num2);//334
+console.log(num3);//100
+console.log(num4);//334
+
