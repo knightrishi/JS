be13ca7fda2a6a66eae916f22f84ba36798b3e84
knightrishi <arnav.ara05@gmail.com>
2025-03-12 18:46:51 +0000

    datatype

---
+++ JS_1/datatype.js
@@ -0,0 +1,20 @@
+"use strict";//treat js as new JS
+//alert("WARNING") 
+//alert(3+2)//use are using nodejs not browser
+/*
+number=2^53
+bigint
+boolean---true ,, false
+
+null =its a stand alone value its like empty value like in SQL
+undefined- jsut like garbage value of c++
+symbol==to find uninqueness 
+
+
+*/ 
+/** object
+ * 
+ * 
+ */
+let age=null
+console.log(typeof age)
