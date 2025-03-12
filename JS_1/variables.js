ce20a3270ed9e90157abae79adfb808cfdda173a
knightrishi <arnav.ara05@gmail.com>
2025-03-12 18:46:41 +0000

    variables

---
+++ JS_1/variables.js
@@ -0,0 +1,11 @@
+const account_id=1445;
+let account_email="arnav.ara"
+var num="23455"
+accountCity="jaipur"
+// account_id=2;
+/*prefer not use var because of issue with block scope and fumction scope */
+account_email="arkk"
+num=2133
+accountCity="ara"
+console.log(account_id)
+console.table([account_id,account_email,num,accountCity])
