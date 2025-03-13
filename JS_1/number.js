7ee5fadc2a3b199e3d592652dc6072aecdb9e14e
knightrishi <arnav.ara05@gmail.com>
2025-03-13 18:15:26 +0000

    number

---
+++ JS_1/number.js
@@ -0,0 +1,26 @@
+const score=33
+
+const balance=new Number(100)
+//console.log(balance);
+//console.log(score);
+
+//console.log(balance.toString());
+//console.log(balance.toFixed(1));
+const otherNum=1122.78765
+//console.log(otherNum.toPrecision(3));//priority decimal ke phele milta hai
+
+const num=1000000
+//console.log(num.toLocaleString(`en-IN`));
+/** ***********************MATHS******** */
+console.log(Math);
+
+//console.log(Math.abs(-33));
+//console.log(Math.round(4.44));
+//console.log(Math.ceil(4.44));
+//console.log(Math.floor(4.44));
+console.log((Math.random()*10) +1);//val always b/w 0-1
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random() *(max-min +1)) + min)
