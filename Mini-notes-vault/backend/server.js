ae98b197c712f5e8205f0cc0611117941d5f6a75
knightrishi <arnav.ara05@gmail.com>
2025-11-15 20:04:21 +0530

    Add basic middleware and health check route to backend server

--- Mini-notes-vault/backend/server.js
+++ Mini-notes-vault/backend/server.js
@@ -1,5 +1,5 @@
 const express=require("express")
-const fs=require("fs")
+
 const app=express();
 const { connectingMongoDB }=require("./db");
 const { json } = require("stream/consumers");
@@ -8,7 +8,8 @@ connectingMongoDB("mongodb://127.0.0.1:27017/mini-notes-vault").then(()=>{
     console.log("DB Connected")
 })
 
-app.use(express.json())//.use is the middleware loader Middleware is just a function that sits between the request and the response.
+app.use(express.json())
+//.use is the middleware loader Middleware is just a function that sits between the request and the response.
 
 app.get("/", (req,res)=>{
     res.send("Mini Notes Vault API running...")
