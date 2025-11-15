49f0dc2192870b208afd3b945634ec7c3d2f710e
knightrishi <arnav.ara05@gmail.com>
2025-11-15 20:01:10 +0530

    Setup backend structure with Express, MongoDB connection, and base routes

--- Mini-notes-vault/backend/server.js
+++ Mini-notes-vault/backend/server.js
@@ -1,12 +1,18 @@
 const express=require("express")
 const fs=require("fs")
 const app=express();
-const { connectingMongoDB }=require("./db")
+const { connectingMongoDB }=require("./db");
+const { json } = require("stream/consumers");
 
 connectingMongoDB("mongodb://127.0.0.1:27017/mini-notes-vault").then(()=>{
     console.log("DB Connected")
 })
 
+app.use(express.json())//.use is the middleware loader Middleware is just a function that sits between the request and the response.
+
+app.get("/", (req,res)=>{
+    res.send("Mini Notes Vault API running...")
+})
  
 const PORT=7070
 
