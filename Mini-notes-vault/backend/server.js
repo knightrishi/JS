4949ae7e1416c01f0b5eb74b71417dcba715ea09
knightrishi <arnav.ara05@gmail.com>
2025-11-16 12:32:58 +0530

    Added Middleware

--- Mini-notes-vault/backend/server.js
+++ Mini-notes-vault/backend/server.js
@@ -1,20 +1,24 @@
 const express=require("express")
-
+const authRoute=require("./routes/authRoutes")
 const app=express();
 const { connectingMongoDB }=require("./db");
-const { json } = require("stream/consumers");
+
 
 connectingMongoDB("mongodb://127.0.0.1:27017/mini-notes-vault").then(()=>{
     console.log("DB Connected")
 })
 
 app.use(express.json())
+app.use("/api/auth",authRoute)
+
+
 //.use is the middleware loader Middleware is just a function that sits between the request and the response.
 
 app.get("/", (req,res)=>{
     res.send("Mini Notes Vault API running...")
 })
- 
+// console.log("Loaded Routes:", authRoute);
+
 const PORT=7070
 
 
@@ -24,5 +28,5 @@ const PORT=7070
 
 
 app.listen(PORT, ()=>{
-    console.log("SERVER STARTED")
+    console.log("SERVER STARTED on PORT",PORT)
 })
