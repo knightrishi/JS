866e7c4eff84c27a3edebe4b4c8cf1857218756a
knightrishi <arnav.ara05@gmail.com>
2025-11-18 00:49:14 +0530

    Added Login component

--- Mini-notes-vault/mini-notes-vault-frontend/src/App.jsx
+++ Mini-notes-vault/mini-notes-vault-frontend/src/App.jsx
@@ -1,9 +1,9 @@
+
+import Login from "./pages/Login";
 export default function App() {
   return (
-    <div className="p-10 space-y-4">
-      <h1 className="text-5xl font-bold text-red-600">RED</h1>
-      <h1 className="text-5xl font-bold text-blue-600">BLUE</h1>
-      <h1 className="text-6xl font-extrabold text-green-600">GREEN</h1>
+    <div >
+       <Login />
     </div>
   );
 }
